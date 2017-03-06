f2b32e6fe2063288e28e2c8d0d39d52c0682eba5
NadimRai <rai_nadim@yahoo.com>
2017-03-06 10:19:04 -0800

    ability to add stars to index and review

--- app/helpers/rental-popularity.js
+++ app/helpers/rental-popularity.js
@@ -4,7 +4,7 @@ export function rentalPopularity(params) {
   var rental = params[0];
 
   if(rental.get('reviews').get('length') >= 5) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+    return Ember.String.htmlSafe('<i class="fa fa-star" aria-hidden="true"></i>');
   }
 }
 
